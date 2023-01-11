35cd3de76635fe46da004dd9fd4d3b18e032b259
Ilia S <iliasedelkin@gmail.com>
2023-01-11 10:42:17 +0100

    Initial Commit

---
+++ EC_Reflect/EC_ReflectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EC_ReflectApp.swift
+//  EC_Reflect
+//
+//  Created by Ilia Sedelkin on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EC_ReflectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
