e473dfc0fb18f4dc5d5a2d6cf7ad533dbbe12617
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2023-01-12 10:33:17 +0100

    fixed view changes

--- EC_Reflect/NoteReflectionView.swift
+++ EC_Reflect/NoteReflectionView.swift
@@ -1,13 +1,3 @@
-
-//
-//struct ProfileStack_Previews: PreviewProvider {
-//    @State static var pictureCount = 0
-//
-//    static var previews: some View {
-//        ProfileStack(dogProfile: ProfilesViewModel().dogsProfiles[4], pictureCount: $pictureCount, frame: <#CGRect#>)
-//    }
-//}
-
 //
 //  SwiftUIView.swift
 //  EC_Reflect
@@ -19,39 +9,28 @@ import SwiftUI
 
 struct NoteReflectionView: View {
     var date = "10 January, 2023"
-    var notes = "Today the crazy old lady watched TV loud again. But we made a prototype"
+    var notes = "Today the crazy old lady watched TV loud again. But we made a prototype for Challenge."
     var emoji = "😀"
-    
     
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)){
             RoundedRectangle(cornerRadius: 19.0)
                 .fill(.white)
                 .frame(width: 333, height: 141)
-                .shadow(radius: 38, x: 0, y: 0)
+                .shadow(radius: 15, x: 0, y: 0)
+                .opacity(0.5)
             
-       
             VStack(alignment: .leading, spacing: 15){
-                Text(date).font(.system(size: 16, weight: .regular))
-                    .tracking(0.41)
+                Text(date).font(.system(size: 16, weight: .bold))
+                    .opacity(0.5)
                 Text(notes).font(.system(size: 16, weight: .regular))
-                    .tracking(0.41)
                     .frame(width: 250, height: 58)
             }
             .padding()
             
             Text(emoji).font(.system(size: 60, weight: .regular))
                 .frame(maxWidth: 320, maxHeight: 141, alignment: .bottomTrailing)
-    
-                
-
         }
-        
-        
-        
-        
-        
-        
     }
 }
 
@@ -60,4 +39,3 @@ struct SwiftUIView_Previews: PreviewProvider {
         NoteReflectionView()
     }
 }
-
