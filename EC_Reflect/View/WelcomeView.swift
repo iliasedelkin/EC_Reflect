48a1ff644a996cd4ac83328b7efaad217623d7d3
SpaceUnicornWizard <oleg.chebykin@gmail.com>
2023-01-25 12:30:12 +0100

    fixed WelcomeView in project.pbxproj

--- EC_Reflect/View/WelcomeView.swift
+++ EC_Reflect/View/WelcomeView.swift
@@ -12,20 +12,21 @@ struct WelcomeView: View {
     
     @ObservedObject var welcomeVM = WelcomeViewModel()
     
+    
     var body: some View {
         ZStack {
             Color ("bg-color").edgesIgnoringSafeArea(.all)
             VStack (){
                 VStack (spacing: 40){
                     Text("Reflect")
-//                        .font(.custom("Nunito-Regular", size: 50))
+                        .font(.custom("Nunito-Regular", size: 50))
                         .foregroundColor(colorScheme == .light ? .black : .white)
                     
-                    //.padding(.bottom, 40)
+                    
                     Image("emotions")
-                    //.padding(.bottom, 100)
+                    
                     Text("Track your mind")
-//                        .font(.custom("Nunito-Regular", size: 24))
+                        .font(.custom("Nunito-Regular", size: 24))
                         .foregroundColor(colorScheme == .light ? .black : .white)
                         .padding(.bottom, 40)
                 }
@@ -33,26 +34,27 @@ struct WelcomeView: View {
                 Button {
                     print("Sign up")
                     welcomeVM.login()
+                    
                 } label: {
                     Text("Sign up")
-//                        .font(.custom("Nunito-Bold", size: 20))
+                        .font(.custom("Nunito-Bold", size: 20))
                         .foregroundColor(.white)
                         .frame(width: 260, height: 60)
                         .background(Color.accentColor)
                     
-                                .cornerRadius(50)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 50).stroke(Color.black, lineWidth: 1)
-                                    
-                                )
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50).stroke(Color.black, lineWidth: 1)
+                            
+                        )
                     
-                        
+                    
                 }
                 Button {
                     print("Skip for now")
                 } label: {
                     Text("Skip for now")
-//                        .font(.custom("Nunito-Bold", size: 20))
+                        .font(.custom("Nunito-Bold", size: 20))
                         .foregroundColor(colorScheme == .light ? .black : .white)
                 }
                 .padding(.top, 10)
