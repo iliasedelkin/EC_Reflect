8198ad4768397062393c57b4f86138def5633c18
Ilia S <iliasedelkin@gmail.com>
2023-02-07 18:36:43 +0100

    Changed authorisation to FaceID, added AuthView and changed the starting screen of the app to WelcomeView

--- EC_Reflect/View/ProfileView.swift
+++ EC_Reflect/View/ProfileView.swift
@@ -10,13 +10,14 @@ import UserNotifications
 
 struct ProfileView: View {
     @ObservedObject var profileVM: ProfileViewModel
+    @State var isFaceIdOn = UserDefaults.standard.isFaceIdOn
 
     @Environment(\.colorScheme) var colorScheme
     
     
     var body: some View {
         
-        VStack (spacing: 30) {
+        VStack (alignment: .leading, spacing: 30) {
             HStack {
                 Text("Settings")
                     .font(.custom("Nunito-Bold", size: 36))
@@ -36,46 +37,59 @@ struct ProfileView: View {
                 Button {
                     profileVM.requestNotifPermission()
                 } label: {
-                    Text("Provide permissions")
+                    Text("Tap to provide permission")
                 }
                 
             //Showing notification controls
             } else {
-                DatePicker("Daily notification time", selection: $profileVM.notificationTime, displayedComponents: .hourAndMinute)
-                    .onChange(of: profileVM.notificationTime) { _ in
-                        if profileVM.isNotificationOn {
-                            profileVM.addDailyNotifications(daysArray: profileVM.weekdaysForNotification)
-                        }
+                DatePicker("Daily notification time",
+                           selection: $profileVM.notificationTime,
+                           displayedComponents: .hourAndMinute)
+                .onChange(of: profileVM.notificationTime) { _ in
+                    if profileVM.isNotificationOn {
+                        profileVM.addDailyNotifications(daysArray: profileVM.weekdaysForNotification)
                     }
+                }
                 
                 Toggle(isOn: $profileVM.isNotificationOn) {
                     Text("Turn daily notifications on")
-                }.onChange(of: profileVM.isNotificationOn) { value in
+                }
+                .onChange(of: profileVM.isNotificationOn) { value in
                     if profileVM.isNotificationOn {
                         profileVM.addDailyNotifications(daysArray: profileVM.weekdaysForNotification)
-                        
                     } else {
                         profileVM.removeDailyNotifications()
-                        
                     }
                 }
             }
-                
-            Button {
-                print("Log out")
-                profileVM.logout()
-            } label: {
-                Text("Log out")
-                    .font(.custom("Nunito-Bold", size: 18))
-                    .foregroundColor(.white)
-                
-                    .padding(.init(top: 18, leading: 70, bottom: 18, trailing: 70))
-                    .background(Color.accentColor)
-                    .cornerRadius(50)
-                  
+            HStack {
+                Text("Privacy")
+                    .font(.custom("Nunito-Regular", size: 20))
+                    .foregroundColor(colorScheme == .light ? .black : .white)
+                Spacer()
             }
-            .padding(.top, 10)
-            .padding(.leading, 16)
+            Toggle(isOn: $isFaceIdOn) {
+                Text("FaceID Authentification")
+            }.onChange(of: isFaceIdOn) { _ in
+                UserDefaults.standard.isFaceIdOn = isFaceIdOn
+            }
+            
+                
+//            Button {
+//                print("Log out")
+//                profileVM.logout()
+//            } label: {
+//                Text("Log out")
+//                    .font(.custom("Nunito-Bold", size: 18))
+//                    .foregroundColor(.white)
+//
+//                    .padding(.init(top: 18, leading: 70, bottom: 18, trailing: 70))
+//                    .background(Color.accentColor)
+//                    .cornerRadius(50)
+//
+//            }
+//            .padding(.top, 10)
+//            .padding(.leading, 16)
             Spacer()
         }
         .padding()
